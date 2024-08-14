d22db14ae9e307429eac3c7b000ac25655ec38ed
damyoungtechnologies <damyounggraphics@gmail.com>
2024-08-14 15:07:11 +0000

    let me check this for th last time

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -2,21 +2,23 @@ import { Seq } from "immutable";
 
 export default function printBestStudents(object) {
     const seq = Seq(object);
-    
-    const filtered = seq.filter((student) => {
-        return student.score > 70;
-    });
+
+    // Filter students with score > 70
+    const filtered = seq.filter((student) => student.score > 70);
 
     function capFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
+    // Convert the filtered Seq back to a JS object
     const JSObject = filtered.toJS();
 
+    // Capitalize the first and last names
     Object.keys(JSObject).forEach((key) => {
         JSObject[key].firstName = capFirstLetter(JSObject[key].firstName);
         JSObject[key].lastName = capFirstLetter(JSObject[key].lastName);
     });
 
+    // Print the final object to the console
     console.log(JSObject);
 }
